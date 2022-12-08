346b3e2fb8ce17d5b13144ccea806ce2abe63133
Julia Flaszynska <julia.flaszynska.99@gmail.com>
2022-12-08 16:03:12 +0100

    Moduł 9 - aktualizacja

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Random;
+
+@Configuration
+public class CarFactory {
+
+    @Bean
+    public Car getRandomCar() {
+        Car car;
+        Random generator = new Random();
+        long firstDay = LocalDate.of(2022, 01, 01).toEpochDay();
+        long lastDay = LocalDate.of(2023, 01, 01).toEpochDay();
+        long day = generator.nextLong(lastDay - firstDay ) + firstDay;
+        LocalDate randomDay = LocalDate.ofEpochDay(day);
+        System.out.println(randomDay);
+        if (randomDay.getMonth() == Month.DECEMBER || randomDay.getMonth() == Month.JANUARY || randomDay.getMonth() == Month.FEBRUARY) {
+            car = new SUV();
+        } else if (randomDay.getMonth() == Month.JUNE || randomDay.getMonth() == Month.JULY || randomDay.getMonth() == Month.AUGUST) {
+            car = new Cabrio();
+        } else {
+            car = new Sedan();
+        }
+        return car;
+    }
+
+
+}
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/SUV.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import java.time.LocalTime;
+
+public class SUV implements Car {
+
+    @Override
+    public boolean hasHeadlightsTurnedOn(LocalTime time) {
+        return time.isAfter(LocalTime.of(19,59,59)) || time.isBefore(LocalTime.of(06,00,00));
+    }
+
+    @Override
+    public String getCarType() {
+        return "SUV";
+    }
+}
