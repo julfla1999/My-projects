fe3d4f83e75c803e662efaceab5be9d8a99b125b
Julia Flaszynska <julia.flaszynska.99@gmail.com>
2023-01-17 02:08:02 +0100

    Zadanie 13.6

---
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/StorePomApplication.java
@@ -0,0 +1,16 @@
+package com.kodilla.selenium.pom.homework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StorePomApplication {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://kodilla.com/pl/test/store");
+        StoreSearchPom searchPom = new StoreSearchPom(driver);
+        searchPom.search("nOTebOoK");
+        searchPom.close();
+    }
+}
